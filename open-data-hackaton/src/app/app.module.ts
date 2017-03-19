fe33f399d34d31e1b020faee6baabd52251efa62
Thomas Gonzalez <gonzalez.thomas@icloud.com>
2017-03-19 15:45:58 +0100

    Intégration template v1

--- open-data-hackaton/src/app/app.module.ts
+++ open-data-hackaton/src/app/app.module.ts
@@ -6,10 +6,20 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import {Ng2BootstrapModule} from "ng2-bootstrap";
 import {SimpleNotificationsModule} from "angular2-notifications";
+import {AppHeaderComponent} from "./app-header/app-header.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {SidebarComponent} from "./sidebar/sidebar.component";
+import { NavbarHeaderComponent } from './navbar-header/navbar-header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    DashboardComponent,
+    SidebarComponent,
+    NavbarHeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
