09a40c83ff0eabb619ece95758b7c553fba71cc5
Thomas Gonzalez <gonzalez.thomas@icloud.com>
2017-03-23 10:39:41 +0100

    Research

--- app/components/search/search.js
+++ app/components/search/search.js
@@ -18,6 +18,16 @@ angular.module('frontProjectApp')
         console.log(error);
       });
 
+    $scope.clickOnCard = function (aCard) {
+      $scope.identifierSelected = aCard.identifier;
+      Request.get(GlobalService.getMetaData + "?id=" + aCard.identifier)
+        .then(function (data) {
+          console.log(data.data[0]);
+          $scope.datasetMeta = data.data[0];
+        }, function (error) {
+          console.log(error);
+        });
+    };
 
     $scope.goDataset = function (id) {
       $location.path('/dataset/' + id);
