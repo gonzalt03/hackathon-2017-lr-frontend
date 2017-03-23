adadc6619dc77eb590d8229e3e9509a6b8d7db28
jcalandr <calandre.jordan@gmail.com>
2017-03-23 08:59:25 +0100

    add map

--- app/components/view/view.js
+++ app/components/view/view.js
@@ -14,36 +14,29 @@ angular.module('frontProjectApp')
           lat: 46.15,
           lng: -1.15,
           zoom: 14
-        },
-        markers: {
-          m1: {
-            lat: 46.15,
-            lng: -1.15,
-            message: "I'm a black one",
-            focus: false,
-            icon: {
-              prefix: 'fa',
-              type: 'awesomeMarker',
-              icon: 'fa-cutlery',
-              markerColor: 'black'
-            }
-          },
-          m2: {
-            lat: 46.145,
-            lng: -1.12,
-            message: "I'm a green marker",
-            focus: false,
-            icon: {
-              prefix: 'fa',
-              type: 'awesomeMarker',
-              icon: 'fa-graduation-cap',
-              markerColor: 'green'
-            }
-          }
         }
       });
 
       $scope.select = function () {
+        leafletData.getMap().then(function (map) {
+          console.log(map);
+          var url = "ca_borne.kml";
+          console.log(url);
+          $scope.rectLayer = omnivore.kml(url).on('ready', function () {
+
+            this.eachLayer(function (marker) {
+                marker.setIcon(L.AwesomeMarkers.icon({
+                  prefix: 'fa',
+                  icon: 'car',
+                  markerColor: 'blue'
+                }))
+              }
+            );
+          })
+            .addTo(map);
+          console.log($scope.rectLayer)
+        });
+
         leafletData.getMap().then(function (map) {
           console.log(map);
           var url = "tr_piste_cyclable.kml";
@@ -63,6 +56,7 @@ angular.module('frontProjectApp')
           console.log($scope.rectLayer)
         });
       };
+
 
       $scope.select();
     }
