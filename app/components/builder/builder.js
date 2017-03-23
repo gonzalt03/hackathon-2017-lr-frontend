a03a1ef847ce784c0c99759600de27b814d4a2c5
jcalandr <calandre.jordan@gmail.com>
2017-03-23 10:00:39 +0100

    add cart to builder

--- app/components/builder/builder.js
+++ app/components/builder/builder.js
@@ -5,7 +5,7 @@
 'use strict';
 
 angular.module('frontProjectApp')
-  .controller('BuilderCtrl', function ($scope, Request, GlobalService) {
+  .controller('BuilderCtrl', function ($scope, Request, GlobalService, leafletData) {
 
     $scope.myTitle = 'LINECHART';
     $scope.myData = sinAndCos();
@@ -152,5 +152,58 @@ angular.module('frontProjectApp')
         location: "La Rochelle"
       }
     ];
+    angular.extend($scope, {
+      defaults: {
+        scrollWheelZoom: false
+      },
+      la_rochelle: {
+        lat: 46.15,
+        lng: -1.15,
+        zoom: 14
+      }
+    });
+
+    $scope.select = function () {
+      leafletData.getMap().then(function (map) {
+        console.log(map);
+        var url = "ca_borne.kml";
+        console.log(url);
+        $scope.rectLayer = omnivore.kml(url).on('ready', function () {
+
+          this.eachLayer(function (marker) {
+              marker.setIcon(L.AwesomeMarkers.icon({
+                prefix: 'fa',
+                icon: 'car',
+                markerColor: 'blue'
+              }))
+            }
+          );
+        })
+          .addTo(map);
+        console.log($scope.rectLayer)
+      });
+
+      leafletData.getMap().then(function (map) {
+        console.log(map);
+        var url = "tr_piste_cyclable.kml";
+        console.log(url);
+        $scope.rectLayer = omnivore.kml(url).on('ready', function () {
+
+          this.eachLayer(function (marker) {
+              marker.setIcon(L.AwesomeMarkers.icon({
+                prefix: 'fa',
+                icon: 'car',
+                markerColor: 'blue'
+              }))
+            }
+          );
+        })
+          .addTo(map);
+        console.log($scope.rectLayer)
+      });
+    };
+
+
+    $scope.select();
 
   });
