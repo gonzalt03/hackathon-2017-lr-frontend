e21d3d1a1f6645a812252b9ee1fbdb5526bf2236
Thomas Gonzalez <gonzalez.thomas@icloud.com>
2017-03-23 04:50:13 +0100

    Recherche et requete

--- app/components/dataset/dataset_details.js
+++ app/components/dataset/dataset_details.js
@@ -6,13 +6,24 @@
 
 
 angular.module('frontProjectApp')
-  .controller('DatasetDetailsCtrl', function ($scope) {
+  .controller('DatasetDetailsCtrl', function ($scope, GlobalService, Request, $routeParams) {
+
+    var id = $routeParams.id;
 
     $scope.dataset = {
       id: 1,
       name: "SIGNALISATION LUMINEUSE – SUPPORT",
       type: "dataset"
     };
+
+    Request.get(GlobalService.getInfoJeuData + "?url="+id)
+      .then(function(data) {
+        console.log(data.data);
+        $scope.dataset.data = data.data;
+        $scope.dataset.header = Object.keys(data.data[0]);
+      }, function(error) {
+        console.log(error);
+      });
 
 
   });
